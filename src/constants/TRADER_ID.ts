152008a907f3aa359c6c93ac294d3f999b5514de
Tyler Mueller <muelltyl@gmail.com>
2024-06-10 18:02:15 -0500

    adds constants

---
+++ src/constants/TRADER_ID.ts
@@ -0,0 +1,10 @@
+export const TRADER_ID = {
+  "MECHANIC": "5a7c2eca46aef81a7ca2145d",
+  "SKIER": "58330581ace78e27b8b10cee",
+  "PEACEKEEPER": "5935c25fb3acc3127c3d8cd9",
+  "THERAPIST": "54cb57776803fa99248b456e",
+  "PRAPOR": "54cb50c76803fa8b248b4571",
+  "JAEGAR": "5c0647fdd443bc2504c2d371",
+  "RAGMAN": "5ac3b934156ae10c4430e83c",
+  "FENCE": "579dc571d53a0658a154fbec"
+} as const;
---
+++ src/constants/mod.ts
@@ -0,0 +1,3 @@
+export * from "./ITEM_ID.ts";
+export * from "./QUEST_ID.ts";
+export * from "./TRADER_ID.ts";
